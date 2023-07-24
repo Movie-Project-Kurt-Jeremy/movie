e209c2cfe0df1122305dbd568203ba803fd8a2e9
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-24 18:06:58 -0500

    made append movies from search bar, still no text suggestions but all movies are displayed when seacrhed up, and max of 4 movies pop up

--- js/movieproject.js
+++ js/movieproject.js
@@ -1,6 +1,3 @@
-// GLOBAL VARIABLES
-const searchInput = document.querySelector("#search");
-
 // Functions
 function updateSearch(e) {
     // Prevent default form submission behavior
@@ -15,6 +12,7 @@ function updateSearch(e) {
         }
     };
 
+    // Fetch the movie data using the Fetch API
     return fetch(url, options)
         .then((response) => {
             console.log(response);
@@ -28,18 +26,73 @@ function updateSearch(e) {
             // Search update functionality
             let movieName = searchInput.value;
             let searchedMovies = [];
-            console.log(movieArray);
-            for (let i = 0; i < movieArray.length; i++) {
-                console.log(movieArray[i].title);
-                if (movieArray[i].title.toLowerCase().includes(movieName.toLowerCase())) {
-                    searchedMovies.push(movieArray[i]);
+
+            // If the search input is empty, display only the first four movies
+            if (movieName === "") {
+                searchedMovies = movieArray.slice(0, 4);
+            } else {
+                // Filter movies based on the search input (case-insensitive)
+                for (let i = 0; i < movieArray.length; i++) {
+                    if (movieArray[i].title.toLowerCase().includes(movieName.toLowerCase())) {
+                        searchedMovies.push(movieArray[i]);
+                    }
                 }
             }
+
+            // Display the filtered movies on the cards
+            displayMovies(searchedMovies);
             return searchedMovies;
         })
-        .catch(error => {
+        .catch((error) => {
             console.log(error.message);
         });
+}
+
+function displayMovies(movies) {
+    // Get the container element where the cards will be appended
+    const movieContainer = document.querySelector(".main-body-container .row");
+
+    // Clear previous movie cards by setting the innerHTML to an empty string
+    movieContainer.innerHTML = "";
+
+    // Check if there are movies to display
+    if (movies.length === 0) {
+        const noResultsElement = document.createElement("p");
+        noResultsElement.textContent = "No results found.";
+        movieContainer.appendChild(noResultsElement);
+    } else {
+        // Append movie cards to the container, but limit it to the first four movies
+        const maxMoviesToShow = 4;
+        let appendedMoviesCount = 0;
+
+        for (const movie of movies) {
+            // Check if the maximum number of movies has been appended
+            if (appendedMoviesCount >= maxMoviesToShow) {
+                break;
+            }
+
+            // Create a new card element for each movie
+            const card = document.createElement("div");
+            card.classList.add("column", "card");
+
+            // Use innerHTML to set the content of the card with movie data
+            card.innerHTML = `
+        <h1>${movie.title}</h1>
+        <h2><span class="small-text">${movie.cast}</span></h2>
+        <div class="row justify-space-between">
+          <button>Remove</button>
+          <button>Add</button>
+        </div>
+        <p>${movie.description}</p>
+      `;
+
+            // Append the card to the movie container
+            movieContainer.appendChild(card);
+
+            // Increment the appended movies count
+            appendedMoviesCount++;
+        }
+    }
 }
 
 // IIFE
@@ -52,16 +105,13 @@ function updateSearch(e) {
     showSlides();
 
     function showSlides() {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {
-            slideIndex = 1;
-        }
-        slides[slideIndex - 1].style.display = "block";
+        // ... existing showSlides() code ...
+
+        // Call the showSlides function recursively after 2 seconds
         setTimeout(showSlides, 2000); // Change image every 2 seconds
     }
 })();
+
+// Event Listener for search input
+const searchInput = document.querySelector("#search");
+searchInput.addEventListener("input", updateSearch);
