ceda145d6b73144b62496c88d5ed3c4cb6ddc353
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-07-25 13:40:04 -0500

    changed json to json server and made array much smaller

--- js/movieproject.js
+++ js/movieproject.js
@@ -1,85 +1,84 @@
 // Functions
 function updateSearch(e) {
-	// Prevent default form submission behavior
-	// e.preventDefault();
+    // Prevent default form submission behavior
+    // e.preventDefault();
 
-	// fetch array
-	let url = '../data/db.json';
-	let options = {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	};
+    // fetch array
+    let url = 'http://localhost:3000/movies';
+    let options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    // Fetch the movie data using the Fetch API
+    return fetch(url, options)
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data[0].title);
+            return data;
+        })
+        .then((movieArray) => {
+            // Search update functionality
+            console.log(movieArray);
+            let movieName = searchInput.value;
+            let searchedMovies = [];
 
-	// Fetch the movie data using the Fetch API
-	return fetch(url, options)
-		.then((response) => {
-			console.log(response);
-			return response.json();
-		})
-		.then((data) => {
-			console.log(data[0].title);
-			return data;
-		})
-		.then((movieArray) => {
-			// Search update functionality
-			console.log(movieArray);
-			let movieName = searchInput.value;
-			let searchedMovies = [];
+            // If the search input is empty, display no movies
+            if (movieName === "") {
+                searchedMovies = movieArray.slice(0, 0);
+            } else {
+                // Filter movies based on the search input (case-insensitive)
+                for (let i = 0; i < movieArray.length; i++) {
+                    if (movieArray[i].title.toLowerCase().includes(movieName.toLowerCase())) {
+                        searchedMovies.push(movieArray[i]);
+                    }
+                }
+            }
 
-			// If the search input is empty, display no movies
-			if (movieName === "") {
-				searchedMovies = movieArray.slice(0, 0);
-			} else {
-				// Filter movies based on the search input (case-insensitive)
-				for (let i = 0; i < movieArray.length; i++) {
-					if (movieArray[i].title.toLowerCase().includes(movieName.toLowerCase())) {
-						searchedMovies.push(movieArray[i]);
-					}
-				}
-			}
-
-			// Display the filtered movies on the cards
-			displayMovies(searchedMovies);
-			return searchedMovies;
-		})
-		.catch((error) => {
-			console.log(error.message);
-		});
+            // Display the filtered movies on the cards
+            displayMovies(searchedMovies);
+            return searchedMovies;
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
 }
 
 function displayMovies(movies) {
-	// Get the container element where the cards will be appended
-	const movieContainer = document.querySelector(".append-cards .row");
+    // Get the container element where the cards will be appended
+    const movieContainer = document.querySelector(".append-cards .row");
 
-	// Clear previous movie cards by setting the innerHTML to an empty string
-	movieContainer.innerHTML = "";
+    // Clear previous movie cards by setting the innerHTML to an empty string
+    movieContainer.innerHTML = "";
 
-	// Check if there are movies to display
-	if (movies.length === 0) {
-		const noResultsElement = document.createElement("p");
-		noResultsElement.textContent = "";
-		noResultsElement.classList.add("noResults")
-		noResultsElement.classList.add("text-white")
-		movieContainer.appendChild(noResultsElement);
-	} else {
-		// Append movie cards to the container, but limit it to the first 4 movies
-		const maxMoviesToShow = 4;
-		let appendedMoviesCount = 0;
+    // Check if there are movies to display
+    if (movies.length === 0) {
+        const noResultsElement = document.createElement("p");
+        noResultsElement.textContent = "";
+        noResultsElement.classList.add("noResults")
+        noResultsElement.classList.add("text-white")
+        movieContainer.appendChild(noResultsElement);
+    } else {
+        // Append movie cards to the container, but limit it to the first 4 movies
+        const maxMoviesToShow = 4;
+        let appendedMoviesCount = 0;
 
-		for (const movie of movies) {
-			// Check if the maximum number of movies has been appended
-			if (appendedMoviesCount >= maxMoviesToShow) {
-				break;
-			}
+        for (const movie of movies) {
+            // Check if the maximum number of movies has been appended
+            if (appendedMoviesCount >= maxMoviesToShow) {
+                break;
+            }
 
-			// Create a new card element for each movie
-			const card = document.createElement("div");
-			card.classList.add("column", "card");
+            // Create a new card element for each movie
+            const card = document.createElement("div");
+            card.classList.add("column", "card");
 
-			// Use innerHTML to set the content of the card with movie data
-			card.innerHTML = `
+            // Use innerHTML to set the content of the card with movie data
+            card.innerHTML = `
         <div class="max-img-size">
         <img src="${movie.thumbnail}" alt="img" style="" class="height">
         </div>
@@ -92,37 +91,44 @@ function displayMovies(movies) {
         
       `;
 
-			// Append the card to the movie container
-			movieContainer.appendChild(card);
+            // Append the card to the movie container
+            movieContainer.appendChild(card);
 
-			// Increment the appended movies count
-			appendedMoviesCount++;
-		}
-	}
+            // Increment the appended movies count
+            appendedMoviesCount++;
+        }
+    }
 }
 
 
-function displayFavoriteMovies(favoriteMovies){
-	const favoriteMovieContainer = document.querySelector(".append-favorite-movies .row");
-	favoriteMovieContainer.innerHTML = "";
+// function displayFavoriteMovies(favoriteMovies){
+// 	const favoriteMovieContainer = document.querySelector(".append-favorite-movies .row");
+// 	favoriteMovieContainer.innerHTML = "";
 
 
-}
+
 // displayFavoriteMovies()
+
+// GLOBAL VARIABLES
+
+
+// EVENTS
+
+
 // IIFE
 (() => {
-	// Call the function to fetch and update the movie array
-	updateSearch();
+    // Call the function to fetch and update the movie array
+    updateSearch();
 
-	// Slide
-	let slideIndex = 0;
-	showSlides();
+    // Slide
+    let slideIndex = 0;
+    showSlides();
 
-	function showSlides() {
+    function showSlides() {
 
-		// Call the showSlides function recursively after 2 seconds
-		setTimeout(showSlides, 2000); // Change image every 2 seconds
-	}
+        // Call the showSlides function recursively after 2 seconds
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
 })();
 
 // Event Listener for search input
